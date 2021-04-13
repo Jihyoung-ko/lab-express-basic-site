7e81626667264be990e0d9033d325e53c5c9507a
koji <koji4386@gmail.com>
2021-04-13 14:05:15 +0200

    express basic site done

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/about', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+app.get('/works', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/views/works.html'));
+});
+
+app.listen(5000, () => {
+  console.log('Server listening on port 3000');
+});
